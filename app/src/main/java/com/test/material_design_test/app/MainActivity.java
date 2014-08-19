2c459a5b2b851c1bde6ea8944c64c85ab736c95a
Alexander Kettler <alexanderkettler@bootcamp-pc.speedport_w_723v_1_32_000>
2014-08-19 13:50:34 +0200

    Initial commit

---
+++ app/src/main/java/com/test/material_design_test/app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.test.material_design_test.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        getActionBar().hide();
+    }
+}
